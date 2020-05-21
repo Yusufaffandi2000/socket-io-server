cf7f50da7426f2f315ca689b051b34a8f886f7cc
Yusufaffandi2000 <42926316+Yusufaffandi2000@users.noreply.github.com>
2020-05-21 07:48:41 +0700

    update
    server

--- express/server.js
+++ express/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+var io = require('socket.io')(http);
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -16,7 +17,7 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../socket-io-form.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
